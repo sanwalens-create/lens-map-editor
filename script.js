d9af6e9dd276451f084203b3c1942f54f3aa424b
sanwalens-create <sanwalens@gmail.com>
2026-07-16 16:15:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -273,7 +273,7 @@ function drawLensBase(ctx, x, y, size, clearFirst = false) {
   const cx = x + size / 2;
   const cy = y + size / 2;
   const r = size * 0.40;
-  const fontSize = size * 0.048;
+  const fontSize = size * 0.045;
   const labelOffset = size * 0.060;
   const tick = size * 0.021;
 
